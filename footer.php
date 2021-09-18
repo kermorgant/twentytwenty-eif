b4430f63650a1621d5c2c4d34133537dec8e51f2
Mikael Kermorgant <mikael@tori.fi>
2021-09-18 22:33:25 +0300

    fixup! update style and add js after footer

--- footer.php
+++ footer.php
@@ -56,10 +56,12 @@
 		<?php wp_footer(); ?>
 
         <script type="text/javascript">
-         if (typeof _paq !== "undefined") {
-             _paq.push(['trackPageView']);
-             _paq.push(['trackVisibleContentImpressions']);
-         }
+         jQuery.ready(function() {
+             if (typeof _paq !== "undefined") {
+                 _paq.push(['trackPageView']);
+                 _paq.push(['trackVisibleContentImpressions']);
+             }
+         });
         </script>
 	</body>
 </html>
